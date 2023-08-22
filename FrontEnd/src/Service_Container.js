c07e85c892e6ee474782bdde274e6a1368db4d03
Odia Adodo <subzbelow@gmail.com>
2023-08-22 16:35:38 +0100

    included jdoc for documentation

--- FrontEnd/src/Service_Container.js
+++ FrontEnd/src/Service_Container.js
@@ -1,6 +1,12 @@
-import awilix, { createContainer, asClass } from "awilix"; //asValue
+import awilix, { createContainer, asClass } from "awilix"; //asValue 
 const sales_Service = require("./Services/sales_Service.js");
 
+/**
+ * Calculates the sum of two numbers.
+ *
+ * @param {sales_Service} a - Injected Sevice into DI container.
+ * @returns {service_container} service Container.
+ */
 const service_container = createContainer({
   injectionMode: awilix.InjectionMode.PROXY
 });
@@ -10,3 +16,4 @@ service_container.register({
 });
 
 module.exports = service_container;
+//using jsdoc jsdoc *.js or jsdoc file1.js file2.js file3.jsfor documentation.
