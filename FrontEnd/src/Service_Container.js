a1fdad325ef30843d76ef34857a86dbfe821a08b
Odia Adodo <subzbelow@gmail.com>
2023-08-22 18:01:10 +0100

    DI

--- FrontEnd/src/Service_Container.js
+++ FrontEnd/src/Service_Container.js
@@ -2,8 +2,7 @@ import awilix, { createContainer, asClass } from "awilix"; //asValue
 const sales_Service = require("./Services/sales_Service.js");
 
 /**
- * Calculates the sum of two numbers.
- *
+ * Container to Inject Dependencies for services.
  * @param {sales_Service} a - Injected Sevice into DI container.
  * @returns {service_container} service Container.
  */
@@ -16,4 +15,4 @@ service_container.register({
 });
 
 module.exports = service_container;
-//using jsdoc jsdoc *.js or jsdoc file1.js file2.js file3.jsfor documentation. jsdoc --version {npm install -g jsdoc}
+//using jsdoc jsdoc *.js or jsdoc file1.js file2.js file3.js for documentation. jsdoc --version {npm install -g jsdoc}
